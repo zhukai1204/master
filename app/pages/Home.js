cbac9a21931859a8a7006ca7c9e60cfd16bc7bc0
朱凯 <zhukai@zhukaideMacBook-Air.local>
2017-08-17 01:23:45 +0800

    add img

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Image, Text, Linking, View ,Slider} from 'react-native';
+import { StyleSheet, Image, Text, Linking, View ,Slider, Dimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Swiper from 'react-native-swiper';
 
@@ -12,17 +12,22 @@ class Home extends React.Component {
 
   render() {
     return (
-      	<Swiper style={styles.wrapper} showsButtons={true}>
-	        <View style={styles.slide1}>
-	          <Text style={styles.text}>Hello Swiper</Text>
-	        </View>
-	        <View style={styles.slide2}>
-	          <Text style={styles.text}>Beautiful</Text>
-	        </View>
-	        <View style={styles.slide3}>
-	          <Text style={styles.text}>And simple</Text>
-	        </View>
-	      </Swiper>
+      	<View style={{flex:1,flexDirection: 'column'}}>
+	      	<Swiper style={styles.wrapper}  autoplay={true} loop={true}>
+		        <View style={styles.slide1}>
+		          <Image source={require('../img/1.jpg')}/>
+		        </View>
+		        <View style={styles.slide2}>
+		          <Image source={require('../img/2.jpg')}/>
+		        </View>
+		        <View style={styles.slide3}>
+		          <Image source={require('../img/3.jpg')}/>
+		        </View>
+		     </Swiper>
+		     <View style={{flex:1,flexDirection: 'column'}}>
+
+		     </View>
+		</View>
     );
   }
 }
@@ -31,21 +36,25 @@ var styles = StyleSheet.create({
   wrapper: {
   },
   slide1: {
+  	flexDirection: 'row',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#9DD6EB',
+    height:280
   },
   slide2: {
+  	flexDirection: 'column',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#97CAE5',
   },
   slide3: {
+  	flexDirection: 'column',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#92BBD9',
   },
   text: {
